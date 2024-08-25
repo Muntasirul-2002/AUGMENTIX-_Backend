56d92430718b2d323a0759668f8da89310260220
muntasirul <muntasirul.msd@gmail.com>
2024-08-25 16:52:39 +0530

    server file updated

--- server.js
+++ server.js
@@ -2,13 +2,37 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import todoRoutes from './routes/todoRoutes.js';
-
+import cors from 'cors'
 dotenv.config();
 connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://augmentix-backend.onrender.com/",
+    "https://augmentix-backend.onrender.com",
+    "http://augmentix-backend.onrender.com",
+    "https://augmentix-todo.vercel.app/",
+    "https://augmentix-todo.vercel.app",
+    "http://augmentix-todo.vercel.app",
+    undefined,
+  ];
+  
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Not allowed by CORS from origin : ${origin}`));
+      }
+    },
+    credentials: true,
+  };
+  
 // Middleware
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
